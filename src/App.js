237b0b9023c1ca41844f07f5986f1694bdcadca8
weljeni <alsunurlanova77@gmail.com>
2022-03-31 16:16:27 +0600

    Contact

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import 'antd/dist/antd.css';
-import Contact from "./components/Contact/Contact";
+
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;600&display=swap');
 </style>
 
 function App() {
   return <div className="App">
-    <Contact />
+
   </div>;
 }
 
